78ee063cc231d5443ba71ea19b21ddd56885760c
mustafatekin93 <60111044+mustafatekin93@users.noreply.github.com>
2022-10-03 16:35:42 +0300

    dialog sistemi eklendi

---
+++ WayToTheBOSS_UnityFiles/Assets/DialogueSystem/DialogueBaseClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+        public bool finished { get; private set; }
+
+        protected IEnumerator WriteText(string input, TMP_Text textHolder, float delay, AudioClip sound)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                textHolder.text += input[i];
+                SoundManager.instance.PlaySound(sound);
+                yield return new WaitForSeconds(delay);
+            }
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.C));
+            finished = true;
+        }
+    }
+}
---
+++ WayToTheBOSS_UnityFiles/Assets/DialogueSystem/DialogueLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private TMP_Text textHolder;
+        private Image characterImage;
+        [SerializeField][TextArea(2, 8)] private string dialogueText;
+        [SerializeField] private float delay;
+        [SerializeField] private AudioClip sound;
+        [SerializeField] private Sprite characterSprite;
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(dialogueText, textHolder, delay, sound));
+        }
+
+        private void Awake()
+        {
+            characterImage = GetComponentInChildren<Image>();
+            characterImage.sprite = characterSprite;
+            characterImage.preserveAspect = true;
+            textHolder = GetComponent<TMP_Text>();
+            textHolder.text = "";
+
+
+        }
+    }
+}
