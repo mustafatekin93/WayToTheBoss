edabff4fd3fe78dfd904286004d0a55449774821
mustafatekin93 <60111044+mustafatekin93@users.noreply.github.com>
2022-10-07 01:31:25 +0300

    Random enemy spawn point eklendi

---
+++ WayToTheBOSS_UnityFiles/Assets/EnemySpawnPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private float spawnDistance;
+    [SerializeField] private float respawnDistance;
+
+    GameObject player;
+    GameObject enemy;
+    float distance;
+    bool spawned = false;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        distance = (player.transform.position - transform.position).magnitude;
+
+        if (spawned == false && distance <= spawnDistance)
+        {
+
+            int index = Random.Range(0, prefabs.Length);
+            enemy = (GameObject)Instantiate(prefabs[index], transform.position, Quaternion.identity);
+            spawned = true;
+        }
+
+        else if (distance > respawnDistance && enemy == null)
+        {
+            spawned = false;
+        }
+    }
+}
