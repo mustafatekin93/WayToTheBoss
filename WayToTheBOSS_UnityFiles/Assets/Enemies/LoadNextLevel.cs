fbea2c7fda0a8fd25827baacf18097477100fea3
mustafatekin93 <60111044+mustafatekin93@users.noreply.github.com>
2022-10-09 20:43:58 +0300

    cutscene Skip eklendi

--- WayToTheBOSS_UnityFiles/Assets/Enemies/LoadNextLevel.cs
+++ WayToTheBOSS_UnityFiles/Assets/Enemies/LoadNextLevel.cs
@@ -8,6 +8,11 @@ public class LoadNextLevel : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        LoadScene();
+    }
+
+    public void LoadScene()
+    {
         SceneManager.LoadScene("gameScene");
     }
 }
